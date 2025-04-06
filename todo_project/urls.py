b19a0f021d4bd27b8dc9f0bcd8f980f7362b85fe
subin1826 <subiniringal@gmail.com>
2025-04-06 15:46:50 +0530

    changes

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from todo.views import*
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register(r'todourl',todoviewset,basename='todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls)),
 ]
